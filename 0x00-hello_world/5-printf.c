d0868707801d44c3805ab954390dc447e55e52da
Alex <Alex>
2023-07-13 12:49:26 -0700

    prints exact file using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+  * main - prints "Programming is like building a multilingual puzzle"
+  *
+  * Return: value 0
+  */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
