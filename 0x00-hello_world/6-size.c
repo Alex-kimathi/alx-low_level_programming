e91e9c96370e111fae95925297603e71d470f61a
Alex <Alex>
2023-07-13 13:38:48 -0700

    prints the size of various data types used

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+  * main - prints "Programming is like building a multilingual puzzle"
+  *
+  * Return: value 0
+  */
+
+int main(void)
+{
+	printf("Size of a char: %d byte(s)\n", sizeof(char));
+	printf("Size of an int: %d byte(s)\n", sizeof(int));
+	printf("Size of a long int: %d byte(s)\n", sizeof(int));
+	printf("Size of a long long: %d byte(s)\n", sizeof(long long));
+	printf("Size of a float: %d byte(s)\n", sizeof(float));
+	return (0);
+}
