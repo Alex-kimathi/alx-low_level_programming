3bda922d7146679efd439d606f41604da7f2594c
Alex <Alex>
2023-07-13 12:37:25 -0700

    c program that prints exacts file using puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+  * main - prints "Programming is like building a multilingual puzzle"
+  *
+  * Return: value 0
+  */
+
+int main(void)
+{
+	char ch[60] = "Programming is like building a multilingual puzzle";
+
+	puts(ch);
+
+	return (0);
+}
