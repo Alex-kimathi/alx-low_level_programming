571db19283a4216482548ccb86d6f201cd4288e3
Alex <Alex>
2023-07-14 14:07:01 -0700

    prints alphabets in lower case

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+  * main - prints alphabets in lower case
+  *
+  * Description: allowed to use putchar only
+  *
+  * Return: (0) success
+  */
+
+int main(void)
+{
+	char a = 97;
+
+	while (a < 123)
+	{
+		putchar(a);
+		a++;
+		putchar('\n');
+	}
+	return (0);
+}
