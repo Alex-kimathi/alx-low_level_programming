7a328c95792df7c4c2de4b25dced300a8ce2b890
Alex <Alex>
2023-07-18 14:09:17 -0700

    prints abc to z

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+  * main - Entry point
+  *
+  * Description: prints lowercase aplhabet
+  *
+  * Return: 1 success or 0 fail
+  */
+int main(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+
+	return (0);
+}
