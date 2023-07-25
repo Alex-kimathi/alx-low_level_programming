9bee95613f116423fdf3c6d3aae796a48622fba1
Alex <Alex>
2023-07-25 13:05:22 -0700

    lenght of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include<main.h>
+
+/**
+  * _strlen - prints lenths of a string
+  *@s: character to be tested
+  *
+  * Return: 0 success
+  */
+int _strlen(char *s)
+{
+	size_t length = 0;
+
+	while (*s++)
+
+	{
+
+		length++;
+	}
+	return (length);
+}
