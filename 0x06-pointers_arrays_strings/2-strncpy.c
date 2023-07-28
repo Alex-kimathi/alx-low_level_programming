5fe9a3ef5aabba67e3218ad71dd722df7713ea24
Alexkimathi <alex2kimathi@gmail.com>
2023-07-28 19:37:59 +0000

    prints a copy of string

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,4 +1,4 @@
-#include <main.h>
+#include "main.h"
 /**
   * _strncpy - copies string from source to destination
   * @dest: destination string
@@ -11,8 +11,12 @@ char *_strncpy(char *dest, char *src, int n)
 	int i;
 
 	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
 		dest[i] = src[i];
+	}
 	for (; i < n; i++)
+	{
 		dest[i] = '\0';
+	}
 	return (dest);
 }
