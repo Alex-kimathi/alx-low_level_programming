d495ffb26e401237af7e57a6f2589f483e272e67
Alexkimathi <alex2kimathi@gmail.com>
2023-07-28 20:36:19 +0000

    converts lowercase to uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+  * string_toupper - changes lowercase to upper case
+  * @str: string to be converted
+  * 
+  * Return: 0 success
+  */
+char *sting_toupper(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] = str[i] - ('a' - 'A');
+		}
+		i++;
+	}
+
+	return (str);
+}
