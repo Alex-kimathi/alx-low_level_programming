467e09debf49897c70847b80b970c1d299aea93b
Alexkimathi <alex2kimathi@gmail.com>
2023-07-28 19:25:49 +0000

    prints a copy of string

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -4,10 +4,12 @@
   * @dest: destination string
   * @src: source string
   * @n: bytes to be copied from source string
+  * Return: dest
   */
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
+
 	for (i = 0, i < n && src[i] != '\0'; i++)
 		dest[i] = src[i];
 	for (; i < n; i++)
