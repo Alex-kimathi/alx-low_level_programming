39c0b376fa2d842aa85ad9f8be3f1d8ab50b7610
Alexkimathi <alex2kimathi@gmail.com>
2023-08-01 17:36:38 +0000

    fills memory

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+  * _memset - Fills the memory
+  *
+  * @b: bytes to fill the memory
+  * @n: number of times
+  * @s: memory area
+  * Return: s success
+  */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	for (unsigned int i = 0; i < n; i++)
+	{
+		s[i] = b;
+		return (s);
+	}
+}
