887448883e84b768cec1acf0efbae09e2a57fa6b
Alexkimathi <alex2kimathi@gmail.com>
2023-08-01 19:44:58 +0000

    checks for a string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+  * _strchr - locates a character string
+  * @c: charcater to be located
+  * @s: string to find character
+  * Return: null or character
+  */
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if *s == c
+		{
+			return (s);
+		}
+		s++;
+	}
+	return (NULL);
+}
