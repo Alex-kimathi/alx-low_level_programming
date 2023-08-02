188500bde9ce069d88140b2086fc62e62f3c0b4e
Alexkimathi <alex2kimathi@gmail.com>
2023-08-02 21:18:31 +0000

    recursions

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+  * _puts_recursion - prints a strings
+  * @s: string
+  * Return: strinng
+  */
+
+void _puts_recursion(char *s)
+{	
+
+	if (*s == '\0')
+	{
+		_putchar('\0');
+		return ;
+	}
+
+	_putchar(*s);
+	 _puts_recursion(s + 1);
+
+
+}
