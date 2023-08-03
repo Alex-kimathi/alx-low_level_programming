b3292b1184ffce92a760aa5d9c693b47650c6925
Alexkimathi <alex2kimathi@gmail.com>
2023-08-03 19:43:19 +0000

    prints a string followed by new line

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,16 +6,16 @@
   */
 
 void _puts_recursion(char *s)
-{	
+{
 
 	if (*s == '\0')
 	{
-		_putchar('\0');
-		return ;
+		_putchar('\n');
+		return;
 	}
 
 	_putchar(*s);
-	 _puts_recursion(s + 1);
+	_puts_recursion(s + 1);
 
 
 }
