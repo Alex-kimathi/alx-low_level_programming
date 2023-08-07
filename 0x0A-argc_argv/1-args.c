d5d747f3e7303d2640de82578505172d48c0cdd1
Alex-kimathi <alex2kimathi@gmail.com>
2023-08-07 19:16:02 +0000

    print counts

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+  * main - count using argc
+  * @argc: arguements integer
+  * @argv: arguement character array
+  * Return: its name
+  */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+/*	for (i = 0; i < argc; i++)*/
+	{
+
+	printf("%d\n", argc-1);
+	}
+	return (0);
+}
+
