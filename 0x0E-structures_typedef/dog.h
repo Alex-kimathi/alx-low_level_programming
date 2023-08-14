091c04c3b2d1049c8c8734db6e7400f42b5faf61
Alex-kimathi <alex2kimathi@gmail.com>
2023-08-14 19:37:24 +0000

    Struct 1

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,14 @@
+/**
+  * struct dog - my sruct
+  * @name: name of char type
+  * @age: float type
+  * @owner: char pointer
+  *
+  * Description: struct that has the above elements
+  */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
