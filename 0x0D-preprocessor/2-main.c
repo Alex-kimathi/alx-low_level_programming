d5d72857390ec407286016e9905dba60666a2db1
Alex-kimathi <alex2kimathi@gmail.com>
2023-08-14 21:15:03 +0000

    prints its name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+
+/**
+  * main - prints its file
+  *
+  * Return: file name
+  */
+int main(void)
+{
+
+	printf("%s\n", __FILE__);
+	return (0);
+}
+#endif
