abd0a36b248aca7cfe31968d7ea3453e8ab19dc3
Alex-kimathi <alex2kimathi@gmail.com>
2023-08-23 13:13:54 +0000

    sum all

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,25 @@
+#include "variadic_functions.h"
+/**
+  * sum_them_all - sums all the arguements
+  * @n: first arguement
+  * Return: 0 succes
+  */
+int sum_them_all(const unsigned int n, ...)
+{
+	int sum = 0;
+	unsigned int i;
+	va_list arg_values;
+
+	va_start(arg_values, n);
+
+	if (n == 0)
+	{
+		return (0);
+	}
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(arg_values, int);
+	}
+	va_end(arg_values);
+	return (sum);
+}
