030bbfe99d56e1db2042e87a00305b0283019427
Alex-kimathi <alex2kimathi@gmail.com>
2023-08-25 14:18:16 +0000

    updated fix

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,4 +1,5 @@
 #include "variadic_functions.h"
+
 /**
   * sum_them_all - sums all the arguements
   * @n: first arguement
@@ -12,14 +13,11 @@ int sum_them_all(const unsigned int n, ...)
 
 	va_start(arg_values, n);
 
-	if (n == 0)
-	{
-		return (0);
-	}
 	for (i = 0; i < n; i++)
-	{
+
 		sum += va_arg(arg_values, int);
-	}
+
 	va_end(arg_values);
+
 	return (sum);
 }
