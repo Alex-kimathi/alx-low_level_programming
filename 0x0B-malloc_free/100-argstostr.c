57555777715860edcf2b4eef5c2f302b8a225f75
Alex-kimathi <alex2kimathi@gmail.com>
2023-08-27 18:15:28 +0000

    advenced

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,49 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+  * argstostr - concatenates all the arguments
+  * @ac: first argument
+  * @av: pointer to concatinated argument
+  * Return: Null if 0, or pointer to string
+  */
+char *argstostr(int ac, char **av)
+{
+	int m, n, o, num1;
+	char *parray;
+
+	num1 = 0;
+	o = 0;
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	m = 0;
+	while (m < ac)
+	{
+		n = 0;
+		while (av[m][n])
+		{
+			num1++;
+			n++;
+		}
+		num1++;
+		m++;
+	}
+	parray = malloc((sizeof(char) * num1) + 1);
+	if (parry == NULL)
+		return (NULL);
+	m = 0;
+	while (m < av)
+	{
+	while (av[m][n])
+	{
+		parray[o] = av[m][n];
+		n++;
+		o++;
+	}
+	parray[o] = '\n';
+	o++;
+	m++;
+	}
+	parray[o] = '\0';
+	return (parray);
+}
