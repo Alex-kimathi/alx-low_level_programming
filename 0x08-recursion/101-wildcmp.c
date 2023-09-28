85ef014b2a919121e06d1b42daa8174f87dc74d9
Alex-kimathi <alex2kimathi@gmail.com>
2023-09-28 15:06:24 +0000

    compares a string

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -12,23 +12,11 @@ int wildcmp(char *s1, char *s2)
 	{
 		return (1);
 	}
-	if (*s2 == '*')
+	if (*s1 == *s2 || *s2 == '*')
 	{
-		s2++;
-		while (*s1 != '\0')
-		{
-			if (wildcmp(s1, s2))
-			{
-				return (1);
-			}
-			s1++;
-		}
-		return (wildcmp(s1, s2));
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
 	}
-	if (*s1 == *s2 || *s2 == '?')
-	{
-		return (wildcmp(s1 + 1, s2 + 1));
-	}
+
 	return (0);
 
 }
