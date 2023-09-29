80814670642288834d2f22976e7f534d108bcf34
Alex-kimathi <alex2kimathi@gmail.com>
2023-09-29 17:04:13 +0000

    prints its name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+  * main - prints its name
+  * 
+  * Return: 0 success
+  */
+int main(int argc, char *argv[])
+{
+	if (argc > 0)
+	{
+		printf("%s\n", argv[0]);
+	}
+	return (0);
+}
