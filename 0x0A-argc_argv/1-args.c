a70e6d70e05bdd607bc8f62edec5f9289ae72c2d
Alex-kimathi <alex2kimathi@gmail.com>
2023-09-29 17:16:16 +0000

    prints number of argurments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+  * main - prints its name
+  *
+  * @argc: counts number of arguements
+  * @argv: array of strings
+  * Return: 0 success
+  */
+int main(int argc, char *argv[])
+{
+	if (argc > 0)
+	{
+		printf("%d\n", argc;
+	}
+	return (0);
+}
