0249ad0769cb407f6369bac9c2dbee45960eb9f1
Alex-kimathi <alex2kimathi@gmail.com>
2023-10-09 18:30:50 +0000

    struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+  * struct dog - structure
+  * Description: creates sipmle elements
+  * @name: 1 element
+  * @age: 2nd element
+  * @owner: 3rd element
+  */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif
